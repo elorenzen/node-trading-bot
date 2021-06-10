853a233a36d43b7be6f3241f69e928e349496088
Eric <eric@kellyklee.com>
2021-06-09 22:56:51 -0700

    Create function for 3-line strike strategy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const init = async () => {
         const now = new Date();
         const start = new Date(now - (2 * oneMinuteMS)).toISOString();
         const end = new Date(now - oneMinuteMS).toISOString();
-        console.log('Checking bars', end, start);
+        console.log('Checking bars for bullish engulfing: ', end, start);
 
         const bars = await getBars({symbol, start, end});
         const bar1 = bars[symbol][0];
@@ -49,7 +49,7 @@ const init = async () => {
             // Buy this stock
             const purchase = await buyMarket({symbol, amt});
             console.log('purchase: ', purchase)
-            
+
             // set stop at bar1 low price
             sellStop({symbol, price: bar1.l, amt});
 
@@ -58,6 +58,61 @@ const init = async () => {
             sellLimit({symbol, price: profitTarget, amt});
         }
     }
+    const threeLineStrike = async () => {
+        // look at last 2 interval candles before the current minute
+        const oneMinuteMS = 60000;
+        const now = new Date();
+        const start = new Date(now - (4 * oneMinuteMS)).toISOString();
+        const end = new Date(now - oneMinuteMS).toISOString();
+        console.log('Checking bars', end, start);
+
+        const bars = await getBars({symbol, start, end});
+        const bar1 = bars[symbol][0];
+        const bar2 = bars[symbol][1];
+        const bar3 = bars[symbol][2];
+        const bar4 = bars[symbol][3];
+
+        // recognize three-line strike
+        if (
+            // Check if bars 1-4 exist
+            (bar1 && bar2 && bar3 && bar4) &&
+            // Is bar1 opening val greater than bar1 closing val?
+            (bar1.o > bar1.c) &&
+            // Is bar2 opening val greater than bar1 closing val?
+            (bar2.o > bar1.c) &&
+            // Is bar2 closing val less than bar2 opening val?
+            (bar2.c < bar2.o) &&
+            // Is bar3 opening val greater than bar2 closing val?
+            (bar3.o > bar2.c) &&
+            // Is bar3 closing val less than bar3 opening val?
+            (bar3.c < bar3.o) &&
+            // Is bar4 lowest val less than bar3 closing val?
+            (bar4.l < bar3.c) &&
+            // Is bar4 high greater than bar1 opening val?
+            (bar4.h > bar1.o) &&
+            // Is bar4 volume higher than bar3 value?
+            (bar4.v > bar3.v)
+        ) {
+            // Get 10% of account buying power
+            const willingToSpend = buyingPower * .1;
+
+            // Find how many shares we can buy with 10% of account value
+            const amt = Math.floor(willingToSpend / bar4.c);
+
+            // Buy this stock
+            const purchase = await buyMarket({symbol, amt});
+            console.log('purchase: ', purchase)
+            
+            // set stop at bar1 low price
+            sellStop({symbol, price: bar3.l, amt});
+
+            // set 100% limit sell at 2:1 profit ratio, which comes out to ((purchase price - stop price) * 2) + purchase price
+            const profitTarget = ((purchase.price - bar3.l) * 2) + purchase.price;
+            sellLimit({symbol, price: profitTarget, amt});
+        }
+    }
+    threeLineStrike();
+    setInterval(threeLineStrike, 60 * 1000);
     checkAndOrder();
     setInterval(checkAndOrder, 60 * 1000);
 }
