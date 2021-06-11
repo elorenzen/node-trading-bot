b5e0a4170dab325bde6665254b9a5cd5fcf10bfd
Eric <eric@kellyklee.com>
2021-06-11 15:12:57 -0700

    Refactor to check for array of array of tickers, (not just one)

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 require('dotenv').config();
-const getDownTrendingStock = require('./lib/getDownTrendingStock');
+const { getStocks, getDownTrendingStock } = require('./lib/getDownTrendingStock');
 const getBars = require('./lib/getBars');
 const getAccountValue = require('./lib/getAccountValue');
 const { buyMarket, sellStop, sellLimit } = require('./lib/order');
 
 const init = async () => {
+    const tickers = await getStocks();
+    console.log('tickers: ', tickers)
     // find downtrending stock
-    const symbol = await getDownTrendingStock();
-    console.log('symbol: ', symbol)
+    // const symbol = await getDownTrendingStock();
+    // console.log('symbol: ', symbol)
     const accountInfo = await getAccountValue()
     const buyingPower = accountInfo.buying_power
     console.log('getAccountValue: ', buyingPower)
 
     // Check every minute
-    const checkAndOrder = async () => {
+    const checkAndOrder = async (ticker) => {
         // look at last 2 interval candles before the current minute
         const oneMinuteMS = 60000;
         const now = new Date();
         const start = new Date(now - (2 * oneMinuteMS)).toISOString();
         const end = new Date(now - oneMinuteMS).toISOString();
-        console.log('Checking bars for bullish engulfing: ', end, start);
+        console.log(`Checking ${ticker} bars for bullish engulfing: `, end, start);
 
-        const bars = await getBars({symbol, start, end});
-        const bar1 = bars[symbol][0];
-        const bar2 = bars[symbol][1];
+        const bars = await getBars({ticker, start, end});
+        const bar1 = bars[ticker][0];
+        const bar2 = bars[ticker][1];
 
         // recognize bullish engulfing pattern
         if (
@@ -47,30 +49,30 @@ const init = async () => {
             const amt = Math.floor(willingToSpend / bar2.c);
 
             // Buy this stock
-            const purchase = await buyMarket({symbol, amt});
+            const purchase = await buyMarket({ticker, amt});
             console.log('purchase: ', purchase)
 
             // set stop at bar1 low price
-            sellStop({symbol, price: bar1.l, amt});
+            sellStop({ticker, price: bar1.l, amt});
 
             // set 100% limit sell at 2:1 profit ratio, which comes out to ((purchase price - stop price) * 2) + purchase price
             const profitTarget = ((purchase.price - bar1.l) * 2) + purchase.price;
-            sellLimit({symbol, price: profitTarget, amt});
+            sellLimit({ticker, price: profitTarget, amt});
         }
     }
-    const threeLineStrike = async () => {
+    const threeLineStrike = async (ticker) => {
         // look at last 2 interval candles before the current minute
         const oneMinuteMS = 60000;
         const now = new Date();
         const start = new Date(now - (4 * oneMinuteMS)).toISOString();
         const end = new Date(now - oneMinuteMS).toISOString();
-        console.log('Checking bars', end, start);
+        console.log(`Checking ${ticker} bars for 3-line strike: `, end, start);
 
-        const bars = await getBars({symbol, start, end});
-        const bar1 = bars[symbol][0];
-        const bar2 = bars[symbol][1];
-        const bar3 = bars[symbol][2];
-        const bar4 = bars[symbol][3];
+        const bars = await getBars({ticker, start, end});
+        const bar1 = bars[ticker][0];
+        const bar2 = bars[ticker][1];
+        const bar3 = bars[ticker][2];
+        const bar4 = bars[ticker][3];
 
         // recognize three-line strike
         if (
@@ -100,20 +102,25 @@ const init = async () => {
             const amt = Math.floor(willingToSpend / bar4.c);
 
             // Buy this stock
-            const purchase = await buyMarket({symbol, amt});
+            const purchase = await buyMarket({ticker, amt});
             console.log('purchase: ', purchase)
             
             // set stop at bar1 low price
-            sellStop({symbol, price: bar3.l, amt});
+            sellStop({ticker, price: bar3.l, amt});
 
             // set 100% limit sell at 2:1 profit ratio, which comes out to ((purchase price - stop price) * 2) + purchase price
             const profitTarget = ((purchase.price - bar3.l) * 2) + purchase.price;
-            sellLimit({symbol, price: profitTarget, amt});
+            sellLimit({ticker, price: profitTarget, amt});
         }
     }
-    threeLineStrike();
-    setInterval(threeLineStrike, 60 * 1000);
-    checkAndOrder();
-    setInterval(checkAndOrder, 60 * 1000);
+    tickers.forEach(ticker => {
+        threeLineStrike(ticker)
+        setInterval(threeLineStrike, 60 * 1000, ticker);
+    })
+
+    tickers.forEach(ticker => {
+        checkAndOrder(ticker);
+        setInterval(checkAndOrder, 60 * 1000, ticker);
+    })
 }
 init();
