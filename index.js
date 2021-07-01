c4b39256454bd1c0e955c9ae9eda4d87ddeaf906
Eric <eric@kellyklee.com>
2021-07-01 02:30:00 -0700

    Create conditional that submits sell limit, sell stop orders for all positions if market is open

--- index.js
+++ index.js
@@ -2,10 +2,42 @@ require('dotenv').config();
 const getStocks = require('./lib/getDownTrendingStock');
 const bullishEngulfing = require('./lib/strategies/bullishEngulfing');
 const threeLineStrike = require('./lib/strategies/threeLineStrike');
+const getPositions = require('./lib/getPositions');
+const { sellStop, sellLimit } = require('./lib/order')
 
 const init = async () => {
     const tickers = await getStocks();
     console.log('tickers: ', tickers)
+    const positions = await getPositions()
+
+    const now = new Date();
+    const hourNow = now.getHours()
+    const minutesNow = now.getMinutes()
+    const dayOfWeek = now.getDay()
+
+    // Market is open
+    if (
+        (hourNow >= 6 && minutesNow >= 30) &&
+        hourNow < 13 &&
+        (dayOfWeek >= 1 && dayOfWeek <= 5)
+    ) {
+        console.log('Market is open!')
+        positions.forEach(position => {
+            // Create sell limit, sell stop order for given symbol
+            console.log('symbol: ', position.symbol)
+            console.log('quantity: ', position.qty)
+            console.log('average entry price: $', position.avg_entry_price)
+            const ticker = position.symbol
+            const amt = position.qty
+
+            const stopPrice = Number(position.avg_entry_price) - (Number(position.avg_entry_price) * .02)
+            sellStop({ticker, price: stopPrice, amt});
+            
+            const profitTarget = (Number(position.avg_entry_price) * 1.2);
+            sellLimit({ticker, price: profitTarget, amt});
+        })
+    }
+    else console.log('Market is closed.')
 
     tickers.forEach(ticker => {
         threeLineStrike(ticker)
